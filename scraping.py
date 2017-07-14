7728cca307eca5cb01b49462a7f46ebf147b2672
Qingyi Wu <Qingyi Wu>
2017-07-13 22:12:47 -0500

    add func parseZpool to parse zpool data and generate profit_dict

--- scraping.py
+++ scraping.py
@@ -1,4 +1,4 @@
-import sys
+import sys, json
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 from PyQt4.QtWebKit import *
@@ -18,6 +18,31 @@ class Render(QWebPage):
   def _loadFinished(self, result):
     self.frame = self.mainFrame()
     self.app.quit()
+
+
+def parseZpool(texts):
+    profit_dict = {}
+    fee_pos = 0
+    with open('algos_zpool.json') as fn:
+        pool_dict = json.load(fn)
+
+    profit_dict = pool_dict
+
+    for i in range(len(texts)):
+        if texts[i] in pool_dict:
+            for j in range(1, 5):
+                if "%" in texts[i+j]:
+                    fee_pos = j                 # fee postion, +3 is profit, -2 is miners
+                    break
+            profit_dict[texts[i]]['normalized_profit'] = float(texts[i+fee_pos+3])
+            profit_dict[texts[i]]['miner_qty'] = texts[i+fee_pos-2]
+            profit_dict[texts[i]]['actual_profit'] = profit_dict[texts[i]]['normalized_profit'] \
+                * pool_dict[texts[i]]['hashrate']
+
+    # with open('profits_zpool.json', 'w') as fn:
+    #     json.dump(pool_dict, fn)
+
+    return profit_dict
 
 
 url = 'http://zpool.ca/'
@@ -45,5 +70,8 @@ data = tree.xpath('///tbody/tr/td[text()]')
 #     pass
 
 txt = [item.text_content() for item in data]
+text = str(''.join(str(txt)))
+with open('zpool_output.txt', 'w') as fn:
+    fn.write(text)
 
-print type(data[0])
+parseZpool(txt)
