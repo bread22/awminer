39aa47cbbcf209377a57cb19f05b54defdc5fb3c
Qingyi Wu <wuqingyi22@gmail.com>
2017-07-14 13:06:18 -0500

    add mining_machine.json as machine config (reverted from commit fccf80604092cd50d1900c2698cec214ffacac48)

--- start.py
+++ start.py
@@ -1,31 +1,18 @@
 __author__ = 'wuqingyi22@gmail.com'
 
-import os, time, json
+import os, time
 import subprocess
 import scraping
 
 pool = 'http://zpool.ca/'
-pool_url_base = '.mine.zpool.ca'
 config = 'algos_zpool.json'
-miner_config = 'mining_machine.json'
 
 zpool = scraping.MiningPool(pool, config)
 
-# print zpool.getTopProfit()
-with open(miner_config) as fn:
-    machine = json.load(fn)
-current_algo = {}
+print zpool.getTopProfit()
 
-while True:
-    top_algo = zpool.getTopProfit()         # get top algo
-    if current_algo:
-        process.kill()                      # kill current miner, it doesn't hurt
-        stratum = 'stratum+tcp://' + top_algo['algo'] + pool_url_base + ':' + top_algo['port']
-        process = subprocess.Popen([top_algo['miner'], '-a', top_algo['algo'], '-o', stratum, '-u',
-                                    machine['wallet'], '-p', machine['name'], 'c=BTC'])
-
-
-    # process = subprocess.Popen(['ping', '-t', '127.0.0.1'])
-    # time.sleep(10)
-    # process.kill()
-    # time.sleep(3)
+# process = subprocess.Popen(['ping', '-t', '127.0.0.1'])
+#
+# time.sleep(10)
+#
+# process.kill()
