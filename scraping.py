6867a71c0232311fe8c4e4246a263a526b15caf6
Qingyi Wu <Qingyi Wu>
2017-07-14 23:54:26 -0500

    skip unused algo to save scraping time, do not print, log to file

--- scraping.py
+++ scraping.py
@@ -16,6 +16,10 @@ class MiningPool(object):
             profit_dict = json.load(fn)
 
         for key in profit_dict:
+            profit_dict[key]['actual_profit'] = 0   # initialize these 2
+            profit_dict[key]['miner_qty'] = 0
+            if profit_dict[key]['hashrate'] == 0 or not profit_dict[key]['miner']:
+                continue                            # skip algo not in use to save some time
             algo = profit_dict[key]['algo']
             url1 = 'http://zpool.ca/site/gomining?algo=' + algo
             url2 = 'http://zpool.ca/site/graph_price_results'
@@ -31,22 +35,18 @@ class MiningPool(object):
             miner_str = wd.find_element_by_class_name('main-left-title').text
             miner_qty = int(re.search(r', (\d+?) miners', miner_str).groups()[0])
             profit_dict[key]['miner_qty'] = miner_qty
-            print profit_dict[key]['algo'], profit_dict[key]['actual_profit'], \
-                profit_dict[key]['normalized_profit']
+            # print profit_dict[key]['algo'], profit_dict[key]['actual_profit'], \
+            #     profit_dict[key]['normalized_profit']
 
-        top_port = ''
-        top_algo = ''
-        top_miner = ''
-        top_profit = 0
+        top_algo = {'profit': 0}
         for key in profit_dict:
-            if profit_dict[key]['actual_profit'] > top_profit and profit_dict[key]['miner_qty'] > 10:
-                top_port = key
-                top_algo = profit_dict[key]['algo']
-                top_profit = profit_dict[key]['actual_profit']
-                top_miner = profit_dict[key]['miner']
+            if profit_dict[key]['actual_profit'] > top_algo['profit'] and profit_dict[key]['miner_qty'] > 10:
+                top_algo['port'] = key
+                top_algo['algo'] = profit_dict[key]['algo']
+                top_algo['profit'] = profit_dict[key]['actual_profit']
+                top_algo['miner'] = profit_dict[key]['miner']
+                top_algo['miner_qty'] = profit_dict[key]['miner_qty']
 
-        print "Algo: ", top_algo
-        print "Port: ", top_port
-        print "Profit: ", top_profit
+        # print top_algo
 
-        return {'algo': top_algo, 'port': top_port, 'profit': top_profit, 'miner': top_miner}
+        return top_algo
